24c894151fc4193d9ae545740ac24bc914df6401
robbert-sudo <robbert_de_Die@hotmail.com>
2022-01-02 16:17:04 +0100

    cors policy aangepast

--- src/main/java/com/example/demo/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -83,6 +83,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/sellerratings").permitAll()
                 .anyRequest().denyAll()
                 .and()
+                .cors()
+                .and()
                 .csrf().disable()
                 //.formLogin().disable()
                 .sessionManagement()
